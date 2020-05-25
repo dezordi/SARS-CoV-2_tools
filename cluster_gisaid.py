5d1b13cecb1384588cec535fb948f5fb73dc8e89
dezordi <zimmer.filipe@gmail.com>
2020-05-25 13:21:54 -0300

    Add cluster_gisaid script and gisaid_reference file

---
+++ cluster_gisaid.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#Write by: Filipe Dezordi (zimmer.filipe@gmail.com)
+#At FioCruz/IAM - 2020/05/25
+
+import argparse, subprocess, shlex, csv, re, os
+parser = argparse.ArgumentParser(description = 'This script automatize cd-hit-est with gisaid sars-cov-sequences by country',formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument("-in", "--input", help="Fasta file.",  required=True)
+parser.add_argument("-rf","--reference",help="Reference file with annotation (region, country, etc).", required=True)
+parser.add_argument("-p","--threads",help="Threads for cd-hit-est analyze.", default = 1, type=int)
+parser.add_argument("-m","--memory",help="Memory in MB for cd-hit-est analyze",default = 2000, type=int)
+args = parser.parse_args()
+sequence_name_file = args.input
+reference_file = args.reference
+var_threads = args.threads
+var_memory = args.memory
+
+input_names = open(sequence_name_file+'.names.txt','w')
+
+with open(sequence_name_file,'r') as input_read:
+    for line in input_read:
+        if '>' in line:
+            input_names.write(line)
+
+input_names.close()
+list_to_cd_hit = []
+
+with open(reference_file,'r') as input_reference, open(sequence_name_file+'.names.txt') as name_reader:
+    list_of_lines_reference = input_reference.readlines()
+    csv_reader = csv.reader(list_of_lines_reference)
+    list_of_lines_name = name_reader.readlines()
+    for line_ref in csv_reader:
+        with open(line_ref[1]+'.names','a') as country_list_file:
+            for line_name in list_of_lines_name:
+                if line_ref[0] in line_name:
+                    line_name = re.sub(r'>','',line_name)
+                    country_list_file.write(line_name)
+        if os.stat(line_ref[1]+'.names').st_size == 0:
+            os.remove(line_ref[1]+'.names')
+        else:
+            if line_ref[1]+'.names' not in list_to_cd_hit:
+                list_to_cd_hit.append(line_ref[1]+'.names')
+            else:
+                pass
+
+seqtk_output_list = []
+
+for file_name in list_to_cd_hit:
+    with open(file_name+'.fasta','w') as seqtk_out:
+        seqtk_cmd = 'seqtk subseq '+sequence_name_file+' '+file_name
+        seqtk_cmd = shlex.split(seqtk_cmd)
+        seqtk_cmd_process = subprocess.Popen(seqtk_cmd,stdout = seqtk_out)
+        seqtk_cmd_process.wait()
+        seqtk_output_list.append(file_name+'.fasta')
+
+for output_name in seqtk_output_list:
+    with open(output_name+'.cd-hit.log','w') as cd_hit_log:
+        cd_hit_out = output_name+'.cd'
+        cd_hit_est_cmd = 'cd-hit-est -i '+output_name+' -o '+cd_hit_out+' -c 1 -s 1 -M '+str(var_memory)+' -T '+str(var_threads)+' -d 200'
+        cd_hit_est_cmd = shlex.split(cd_hit_est_cmd)
+        cd_hit_est_cmd_process = subprocess.Popen(cd_hit_est_cmd,stdout = cd_hit_log)
+        cd_hit_est_cmd_process.wait()
